10bee6b35cb12e228b7ad0703f47b92f171c5142
rahul <rahuljoshi00794@gmail.com>
2022-01-03 16:41:32 +0530

    component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactionModule } from './transaction/transaction.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { SharedModule } from './shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './shared/components/header.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    DashboardModule,
+    TransactionModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
