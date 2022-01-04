77d0b12e4b570a1fd06d9897d5316f8e34f03dba
rahul <rahuljoshi00794@gmail.com>
2022-01-04 21:30:35 +0530

    header changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,8 +8,6 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { SharedModule } from './shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -23,10 +21,8 @@ import { MatIconModule } from '@angular/material/icon';
     TransactionModule,
     RouterModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    MatIconModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
 import { MatInputModule } from "@angular/material/input"
 import { MatIconModule } from "@angular/material/icon"
+import { MatSelectModule } from "@angular/material/select";
+import { MatOptionModule } from "@angular/material/core";
 
 const declarations = [
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule,
+    MatOptionModule
 ]
 
 @NgModule({
