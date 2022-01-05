4a54af885e07b53df59109431dda4e9563fecc8e
Rahul Joshi <rahul.joshi@accolite.com>
2022-01-05 11:54:02 +0530

    footer changes

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  footerList = [
+    {
+      label: "SOCIAL",
+      subList: ["Facebook", "Twitter", "Instagram","Youtube"]
+    },
+    {
+      label: "ABOUT",
+      subList: ["About Us", "Contact Us", "Careers", "Global Presence", "Corporate Information"]
+    },
+    {
+      label: "POLICY",
+      subList: ["Return Policy", "Terms Of Use", "Security", "Privacy"]
+    },
+    {
+      label: "HELP",
+      subList: ["Payments", "Shiping","Cancellation & Returns", "FAQ"]
+
+    }
+
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
