1350305ff0c8e3dd2ce34e388e2f3da27bd94126
Rahul Joshi <rahul.joshi@accolite.com>
2022-01-17 20:01:32 +0530

    description

--- src/app/dashboard/components/product-detail/product-detail.component.ts
+++ src/app/dashboard/components/product-detail/product-detail.component.ts
@@ -11,13 +11,26 @@ export class ProductDetailComponent implements OnInit {
   currentDisplayedImage:string;
   selectedVarient:any;
   selectedColor:any;
+  productInfo:any;
   constructor(private dialogRef:MatDialogRef<ProductDetailComponent>,
     @Inject(MAT_DIALOG_DATA) public dialogData: any ) { }
 
   ngOnInit(): void {
+    this.setInitialData();
+  }
+
+  setInitialData(){
     this.currentDisplayedImage = this.dialogData?.product_image?.[0];
     this.selectedVarient = this.dialogData?.product_varients?.[0];
     this.selectedColor = this.dialogData?.product_varients?.[0]?.colors?.[0];
+    this.fetchAboutProduct();
+  }
+  fetchAboutProduct() {
+    const string = this.dialogData?.product_features;
+    this.productInfo = string?.split('<li>')?.slice(1, this.productInfo?.length);
+    this.productInfo = this.productInfo?.map((item) => {
+      return item?.replace('</li>', '')
+    });
   }
 
   onClickClose(){
